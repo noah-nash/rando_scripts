2e4b9ddb8b2b0e4bd46486c0c2bf28768e3c4efa
Noah Nash <48302704+noah-nash@users.noreply.github.com>
2023-08-20 16:05:23 +0000

    Replace the specific directory path with generic form
    In the format of `<directory>\...`

--- Batch/Jeopardy DVR Process.bat
+++ Batch/Jeopardy DVR Process.bat
@@ -1,8 +1,8 @@
 @echo off
-pushd "\\192.168.1.196\nasty\tvheadend_recording\Jeopardy!"
+pushd "\\<IP address>\tvheadend_recording\Jeopardy!"
 for %%a in (*.ts) do (
 echo processing %%a
-C:\Users\Noah-PC\Downloads\comcutter-main\comcutter_ota.bat "%%~fa" >>"Out_%%~na.txt"
+C:\<path to comcut>\comcutter_ota.bat "%%~fa" >>"Out_%%~na.txt"
 )
-popd "\\192.168.1.196\nasty\tvheadend_recording\Daytime Jeopardy"
+popd "\\<IP address>\tvheadend_recording\Daytime Jeopardy"
 exit /b 0
